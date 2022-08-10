29602ca83b0e656590c8f772b75c8b921d7b193a
SunadaAtaru <sleepingdoll2008@gmail.com>
2022-08-10 23:39:59 +0900

    devise,サインアップ画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users
-   # この行を編集する
-   root 'pages#home'
+   
+   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
